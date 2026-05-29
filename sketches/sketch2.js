18f26447764781d57d5c794a557e072cd34d64de
linr5662 <linr5662@gmail.com>
2026-05-29 14:27:52 -0400

    delete wrong change

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -45,8 +45,6 @@ class Snowflake {
     // Define angular speed (degrees / second)
     let angularSpeed = 35;
 
-    this.posX += sin(time + this.initialAngle) * 0.5;
-
     // Calculate the current angle
     let angle = this.initialAngle + angularSpeed * time;
 
