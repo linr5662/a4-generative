324d37c214743fa26fd8abf9a3e866aa53974f30
linr5662 <linr5662@gmail.com>
2026-05-29 14:22:00 -0400

    Make the snow denser

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -7,7 +7,7 @@ function setup() {
   angleMode(DEGREES);
 
   // Create snowflake objects
-  for (let i = 0; i < 300; i++) {
+  for (let i = 0; i < 700; i++) {
     // Add a new snowflake object to the array
     snowflakes.push(new Snowflake());
   }
