cb269b22451498618d21e24e6e3348aeec781a7f
linr5662 <linr5662@gmail.com>
2026-05-29 20:45:49 -0400

    add 2 sketch to remix

--- sketches/remix.js
+++ sketches/remix.js
@@ -1,14 +1,117 @@
-// =============================================
-// remix.js — your combined sketch
-// This is where sketch1 and sketch2 come together
-// into something new
-// =============================================
+let angle;
+let snowflakes = [];
 
 function setup() {
-  createCanvas(800, 500);
+  createCanvas(800, 600);
+
+  colorMode(HSB);
+  angleMode(DEGREES);
+
+  // Create snowflakes
+  for (let i = 0; i < 600; i++) {
+    snowflakes.push(new Snowflake());
+  }
 }
 
 function draw() {
-  background(220);
-  // your combined code here
+  background(50);
+
+  // TREE -------------------------
+
+  angle = (mouseX / width) * 40;
+  angle = min(angle, 90);
+
+  push();
+
+  translate(width / 2, height);
+
+  stroke(25, 200, 255);
+  line(0, 0, 0, -120);
+
+  translate(0, -120);
+
+  branch(120, 0);
+
+  pop();
+
+  // SNOW -------------------------
+
+  let currentTime = frameCount / 60;
+
+  for (let flake of snowflakes) {
+    flake.update(currentTime);
+    flake.display();
+  }
+}
+
+function branch(h, level) {
+  stroke(level * 25, 255, 255);
+
+  h *= 0.66;
+
+  if (h > 2) {
+    push();
+
+    rotate(angle);
+    line(0, 0, 0, -h);
+    translate(0, -h);
+    branch(h, level + 1);
+
+    pop();
+
+    push();
+
+    rotate(-angle);
+    line(0, 0, 0, -h);
+    translate(0, -h);
+    branch(h, level + 1);
+
+    pop();
+  }
+}
+
+class Snowflake {
+  constructor() {
+    this.posX = 0;
+    this.posY = random(-height, 0);
+    this.initialAngle = random(0, 360);
+    this.size = random(1.5, 4.5);
+    this.radius = sqrt(random(pow(width / 2, 2)));
+
+    this.color = color(
+      random(180, 220),
+      random(200, 256),
+      random(200, 256)
+    );
+  }
+
+  update(time) {
+    let angularSpeed = 35;
+
+    let snowAngle = this.initialAngle + angularSpeed * time;
+
+    this.posX =
+      width / 2 +
+      this.radius * sin(snowAngle) +
+      sin(time + this.initialAngle) * 6;
+
+    let ySpeed = 8 / this.size;
+    this.posY += ySpeed;
+
+    if (this.posY > height) {
+      this.posY = -50;
+    }
+  }
+
+  display() {
+    fill(
+      red(this.color),
+      green(this.color),
+      blue(this.color),
+      140
+    );
+
+    noStroke();
+    ellipse(this.posX, this.posY, this.size);
+  }
 }
